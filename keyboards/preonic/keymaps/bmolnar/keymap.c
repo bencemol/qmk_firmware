55530276611390c2fb1dadaa8bd9d6ae7d065ad0
Bence Molnár <bence.molnar@talkabot.net>
2021-12-29 13:34:59 +0100

    fix missing backspace

--- keyboards/preonic/keymaps/bmolnar/keymap.c
+++ keyboards/preonic/keymaps/bmolnar/keymap.c
@@ -45,8 +45,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_5x12(
-        KC_GESC, KC_1,    KC_2,    KC_3,  KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    TD(TD_LBRC_RBRC_BSLS),
-        KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC,
+        KC_GESC, KC_1,    KC_2,    KC_3,  KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    TD(TD_LBRC_RBRC_BSLS),
         KC_DEL,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
         KC_LCTL, KC_LGUI, KC_LALT, TT(2), MO(3), KC_SPC, KC_SPC, MO(2), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[3] = LAYOUT_ortho_5x12(
         KC_F1, KC_F2,  KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10, KC_F11, KC_F12,
         KC_NO, RESET,  DEBUG,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   TERM_ON, TERM_OFF, KC_NO,  KC_NO,  KC_NO,
-        KC_NO, KC_NO,  MU_MOD, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, DF(0),   DF(4),    KC_NO,  KC_NO,  KC_NO,
+        KC_NO, KC_NO,  RGB_TOG, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, DF(0),   DF(4),    KC_NO,  KC_NO,  KC_NO,
         KC_NO, MUV_DE, MUV_IN, MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  KC_NO,   KC_NO,    KC_NO,  KC_NO,  KC_NO,
         KC_NO, KC_NO,  KC_NO,  KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_NO,  KC_NO
     ),
