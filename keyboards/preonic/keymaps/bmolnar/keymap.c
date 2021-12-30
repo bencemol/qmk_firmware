451e1e37a9a1fb35046f325e0b7dabb1f675717c
Bence Molnar <ce.molnar@hotmail.com>
2021-12-30 14:35:24 +0000

    home row navigation

--- keyboards/preonic/keymaps/bmolnar/keymap.c
+++ keyboards/preonic/keymaps/bmolnar/keymap.c
@@ -61,15 +61,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_MINS, KC_EQL,   KC_BSPC,
         KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,     KC_NO,
         KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT, KC_NO,
-        KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NUHS, KC_NO,    KC_NO,   KC_NO,    KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(3),   KC_MNXT,  KC_VOLD, KC_VOLU,  KC_MPLY
+        KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(3),   KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS
     ),
 	[3] = LAYOUT_ortho_5x12(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,   KC_F12,
-        KC_NO,   RESET,   DEBUG,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   TERM_ON, TERM_OFF, KC_NO,   KC_NO,    KC_NO,
-        KC_NO,   KC_NO,   RGB_TOG, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, KC_NO,   KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,
-        KC_TRNS, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_TRNS, KC_NO,    KC_NO,   RGB_VAI,  RGB_HUI
+        KC_NO,   RESET,   DEBUG,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,
+        KC_TRNS, KC_NO,   RGB_TOG, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_HOME, KC_PGDN,  KC_PGUP, KC_END,   KC_TRNS,
+        KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,
+        KC_TRNS, KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_TRNS, KC_NO,    KC_NO,   RGB_VAI,  RGB_HUI
     )
 };
 
