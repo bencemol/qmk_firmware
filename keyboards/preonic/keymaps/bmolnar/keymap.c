5e9848cb1a3a3b6d4134551d8ef8b8999b1e5db6
Bence Molnar <ce.molnar@hotmail.com>
2022-03-30 13:50:48 +0000

    add default macos layer

--- keyboards/preonic/keymaps/bmolnar/keymap.c
+++ keyboards/preonic/keymaps/bmolnar/keymap.c
@@ -43,13 +43,13 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_ortho_5x12(
+	[0] = LAYOUT_ortho_5x12(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,     KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     TD(TD_LBRC_RBRC_BSLS),
         KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  KC_ENT,
-        KC_LCTL, KC_BTN1, KC_LGUI, KC_RALT, MO(2),   KC_SPC,  KC_SPC,  MO(1),   KC_LEFT,  KC_DOWN, KC_UP,    KC_RGHT
-    ),
+        KC_LCTL, KC_BTN1, KC_RALT, KC_LGUI, MO(2),   KC_SPC,  KC_SPC,  MO(1),   KC_LEFT,  KC_DOWN, KC_UP,    KC_RGHT
+    ),  
 	[1] = LAYOUT_ortho_5x12(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,   KC_F12,
         KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_LBRC, KC_RBRC,  KC_BSLS,
@@ -65,12 +65,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(3),   KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS
     ),
 	[3] = LAYOUT_ortho_5x12(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,   KC_F12,
+        DF(0),   DF(4),   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,   KC_F12,
         KC_NO,   RESET,   DEBUG,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,
         KC_TRNS, KC_NO,   RGB_TOG, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_HOME, KC_PGDN,  KC_PGUP, KC_END,   KC_TRNS,
         KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,
         KC_TRNS, KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_TRNS, KC_NO,    KC_NO,   RGB_VAI,  RGB_HUI
-    )
+    ),
+    [4] = LAYOUT_ortho_5x12(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,     KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     TD(TD_LBRC_RBRC_BSLS),
+        KC_DEL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  KC_ENT,
+        KC_LCTL, KC_BTN1, KC_LGUI, KC_RALT, MO(2),   KC_SPC,  KC_SPC,  MO(1),   KC_LEFT,  KC_DOWN, KC_UP,    KC_RGHT
+    ),
 };
 
 #ifdef RGB_MATRIX_ENABLE
